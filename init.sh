732070e2dec659cf90ddb414122f385d006c9862
j7k6 <mail@j7k6.org>
2019-03-10 20:03:16 +0100

    init.sh

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@
 ln -sf $PWD/.vimrc $HOME/.vimrc
 ln -sf $PWD/.tmux.conf $HOME/.tmux.conf
 ln -sf $PWD/.zshrc $HOME/.zshrc
-cp -rf $PWD/.vim/* $HOME/.vim/ 2>/dev/null
+cp -af $PWD/.vim/* $HOME/.vim/ 2>/dev/null
